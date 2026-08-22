5157be91d246db3a8c43cd729f4499f56bb91404
ahdm13 <ahdminhajc@gmail.com>
2026-08-22 22:25:50 +0530

    New download seutp

--- download.mk
+++
@@ -1,6 +0,0 @@
-DIST_DIR = /var/cache/pkg/dist
-
-dl_cmd = wget -c $(2) -O $(notdir $(1)) -P $(dir $(1))
-
-$(DIST_DIR)/binutils-%.tar.xz:
-	$(call dl_cmd,$@,https://sourceware.org/pub/binutils/releases/$(notdir $@))
